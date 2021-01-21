317a6af6bae5cbb60efc54d5b4459fff9a2fe9e0
aolhovik <toshaolhovik@gmail.com>
2021-01-21 21:58:20 +0200

    Initial commit.

---
+++ src/test/java/com/amakedon/registryservice/RegistryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amakedon.registryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
